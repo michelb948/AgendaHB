233847a35b1cd2d09d5f880d25921fe3430d063f
Michel <124083816+michelb948@users.noreply.github.com>
2023-09-11 14:35:32 -0300

    Add files via upload

---
+++ Tela-de-Login.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+
+    <div class="Usuario">
+        <label for="Usuário">Usuário</label>
+        <input type="text"  name="Usuário" value="0">
+    </div>
+    
+    <br>
+    
+    <div class="Senha">
+        <label for="Senha">Senha</label>
+        <input type="number" name="Senha" value="0">
+    </div>
+
+
+<br>
+
+<div class="Enviar"><input type="submit"></div>
+
+<img src="" alt="Logo do Humberto Bezerra">
+
+
+<style>
+
+    .Usuario{
+        text-align: center;
+        size: 100px;
+    }
+    .Senha{
+        text-align: center;
+        margin-left: 11px;
+    }
+    .Enviar{
+        text-align: center;
+        margin-left: 45px; 
+    }
+    body{
+        background-color: orange;
+    }
+    
+</style>
+
+<?php
+
+while Enviar = true:
+    header('Cadstro.php')
+
+?>
+
+</body>
+</html>
